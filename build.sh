48dcdf0be7cc86f70edbcce76f5ec11ca84e3298
IDKWHATTOCALLTHISISO <a5d3y1@gmail.com>
2025-11-30 11:14:56 -0500

    Specify C standard in gcc command

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ cwd="$(pwd)"
 mkdir /tmp/build && cd /tmp/build
 wget https://img2text.aptjet.dpdns.org/img2txt.c
 wget https://raw.githubusercontent.com/nothings/stb/refs/heads/master/stb_image.h
-gcc -Os img2txt.c -o /usr/local/bin/img2txt -lm
+gcc -Os --std=gnu99 img2txt.c -o /usr/local/bin/img2txt -lm
 cd "$cwd"
 rm -rf /tmp/build
