61c83d53f844563bf77cea8426fae1c070f4680a
IDKWHATTOCALLTHISISO <a5d3y1@gmail.com>
2025-11-30 18:00:51 -0500

    Fix filename typo in build script

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -e
 cwd="$(pwd)"
 mkdir /tmp/build && cd /tmp/build
-wget https://img2text.aptjet.dpdns.org/img2txt.c
+wget https://img2text.aptjet.dpdns.org/img2text.c
 wget https://raw.githubusercontent.com/nothings/stb/refs/heads/master/stb_image.h
-gcc -Os --std=gnu99 img2txt.c -o /usr/local/bin/img2txt -lm
+gcc -Os --std=gnu99 img2text.c -o /usr/local/bin/img2text -lm
 cd "$cwd"
 rm -rf /tmp/build
