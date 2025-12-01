29590a5dae4143b989c6da4ccd9bbfee3c9fe8bd
IDKWHATTOCALLTHISISO <a5d3y1@gmail.com>
2025-12-01 18:19:59 -0500

    Add installation script for img2text
    This script automates the installation of img2text by downloading necessary files and compiling them.

---
+++ slimcc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# install: wget -qO- https://img2text.aptjet.dpdns.org/build.sh | bash
+set -e
+cwd="$(pwd)"
+mkdir /tmp/build && cd /tmp/build
+wget https://img2text.aptjet.dpdns.org/img2text.c
+wget https://raw.githubusercontent.com/nothings/stb/refs/heads/master/stb_image.h
+slimcc -Os img2text.c -o /usr/local/bin/img2text -lm
+cd "$cwd"
+rm -rf /tmp/build
